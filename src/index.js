b1522b1ad9a15dde807c28b5e21c498a5f976d52
Yuliia-Gorgodze <taragorgodze@gmail.com>
2020-12-13 16:33:39 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 
-// import * as one from '../js/task-one'
-// import * as two from '../js/task-two'
+import * as one from '../js/task-one'
+import * as two from '../js/task-two'
 import * as three from '../js/task-three'
